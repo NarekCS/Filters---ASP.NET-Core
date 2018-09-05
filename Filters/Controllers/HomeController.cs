025dfac44a0fa493956c575f56f724240e23d1ec
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-05 12:40:01 +0400

    Filters

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Filters.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Controllers
+{
+    [HttpsOnly]
+    [Profile]
+    [ViewResultDetails]
+    public class HomeController : Controller
+    {        
+        public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
+        
+        public ViewResult SecondAction() => View("Message", "This is the SecondAction action on the Home controller");
+    }
+}

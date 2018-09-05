5e8c0fe8a06ba9e5948895806b419a529373f119
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-05 13:09:54 +0400

    Exception filters

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -11,10 +11,27 @@ namespace Filters.Controllers
     [HttpsOnly]
     [Profile]
     [ViewResultDetails]
+    [RangeException]
     public class HomeController : Controller
     {        
         public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
         
         public ViewResult SecondAction() => View("Message", "This is the SecondAction action on the Home controller");
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
     }
 }
